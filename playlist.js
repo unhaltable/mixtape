477b404043a2e4981009a01397920b15165cb965
Spencer Elliott <me@elliottsj.com>
2014-09-20 12:50:20 -0400

    Stuff

--- playlist.js
+++ playlist.js
@@ -1,19 +1,50 @@
-/*Router.map(function() {
+Router.map(function() {
   this.route('home', {path: '/'});
   this.route('mixtape');
-});*/
+});
+
+if (Meteor.isServer) {
+  var rdio = Meteor.npmRequire('rdio')({
+    rdio_api_key: 'uq3vzfjq8hng3cc7rr7gx92y',
+    rdio_api_shared: process.env.RDIO_SECRET,
+    callback_url: 'http://localhost:3000'
+  });
+
+  Meteor.methods({
+    getPlaybackToken: function () {
+      if (false /*you want to throw an error*/)
+        throw new Meteor.Error(404, "Can't find my pants");
+      return rdio.api();
+    },
+
+    foo: function () {
+      return 'foo';
+    }
+  });
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
 
 if (Meteor.isClient) {
 
+  Meteor.call('foo', function () {
+    console.info(arguments);
+  });
+
+  var playbackToken = Meteor.call('getPlaybackToken');
+  $('#now-playing').rdio(playbackToken);
+
   Template.songs.songs = function () {
     return Songs.find({}, { sort: { upvotes: -1}});
-  }
+  };
 
   Template.songs.events({
     'click input': function () {
       Songs.update(this._id, {$inc: {upvotes: 1}});
     }
-  })
+  });
 
   Template.add_song.events({
     'click input': function () {
@@ -24,7 +55,7 @@ if (Meteor.isClient) {
             Songs.insert({
             name: song_title.value,
             artist: song_artist.value,
-            upvotes: 0,
+            upvotes: 0
           });
       
         document.getElementById('new_song_title').value = '';
@@ -38,9 +69,3 @@ if (Meteor.isClient) {
 
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
- 
