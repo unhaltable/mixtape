26e7ce9d84fecae3f026e42d428b190926d3d96f
canadianbif <benjaminfeder@v1021-wn-92-168.campus-dynamic.uwaterloo.ca>
2014-09-20 04:33:54 -0400

    adding song queue

--- playlist.js
+++ playlist.js
@@ -6,6 +6,10 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.Songs.songs = function () {
+    return Songs.find({}, { sort: { upvotes: 1}});
+  }
+
   Template.messages.messages = function () {
     return Messages.find({}, { sort: { time: -1 }});
   };
