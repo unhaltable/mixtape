895dfcf046b9e981d1e62fcfdb4bce2621bdfe17
aleccool213 <alec_brunelle5@hotmail.com>
2014-09-20 02:19:17 -0400

    chatbox/models

--- playlist.js
+++ playlist.js
@@ -10,6 +10,30 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+
+  Template.messages.messages = function () {
+    return Messages.find({}, { sort: { time: -1 }});
+  }
+
+  Template.input.events = {
+  'keydown input#message' : function (event) {
+      if (event.which == 13) { // 13 is the enter key event
+        var name = 'Anonymous';
+        var message = document.getElementById('message');
+   
+        if (message.value != '') {
+          Messages.insert({
+            name: name,
+            message: message.value,
+            time: Date.now(),
+          });
+   
+          document.getElementById('message').value = '';
+          message.value = '';
+        }
+      }
+    }
+  }
 }
 
 if (Meteor.isServer) {
