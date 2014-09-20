cd3d7ea79a114b0c1d6920304ecdf8944cae1f38
canadianbif <benjaminfeder@v1021-wn-92-168.campus-dynamic.uwaterloo.ca>
2014-09-20 19:29:10 -0400

    added mixtape database

--- models.js
+++ models.js
@@ -1,2 +1,5 @@
-//collection for items in our song queue
+//collection for individual song data
 Songs = new Meteor.Collection('songs');
+
+//collection for individual mixtape data
+Mixtapes = new Meteor.Collection('mixtapes');
