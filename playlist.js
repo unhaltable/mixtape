4ed6edc071a33eabbef3edf8fed8b8fa3c9e14fe
canadianbif <benjaminfeder@v1021-wn-92-168.campus-dynamic.uwaterloo.ca>
2014-09-20 04:55:13 -0400

    song queue shit

--- playlist.js
+++ playlist.js
@@ -7,6 +7,20 @@ if (Meteor.isClient) {
 
   Template.songs.songs = function () {
     return Songs.find({}, { sort: { upvotes: 1}});
+  }
+
+  Template.song_queue.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+
+  /*
+  Template.messages.messages = function () {
+    return Messages.find({}, { sort: { time: -1 }});
+>>>>>>> song queue shit
   };
 
 }
