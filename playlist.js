72623e7e90f0600682953ccf18d83420012e0c39
canadianbif <benjaminfeder@v1021-wn-92-168.campus-dynamic.uwaterloo.ca>
2014-09-20 04:49:08 -0400

    commented out chatbox

--- playlist.js
+++ playlist.js
@@ -10,19 +10,16 @@ if (Meteor.isClient) {
     return Songs.find({}, { sort: { upvotes: 1}});
   }
 
+  /*
   Template.messages.messages = function () {
     return Messages.find({}, { sort: { time: -1 }});
   };
 
+  
   Template.input.events = {
   'keydown input#message' : function (event) {
       if (event.which == 13) { // 13 is the enter key event
-        if(Meteor.user()){
-          var name = Meteor.user().profile.name;
-        }
-        else{
-          var name = 'Anonymous';  
-        }
+        var name = 'Anonymous';  
         var message = document.getElementById('message');
    
         if (message.value != '') {
@@ -38,6 +35,7 @@ if (Meteor.isClient) {
       }
     }
   }
+  */
 }
 
 if (Meteor.isServer) {
