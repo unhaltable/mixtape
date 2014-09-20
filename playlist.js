f2355152edd834a9bd1d2fb38d702c95fc7f5ec2
Spencer Elliott <me@elliottsj.com>
2014-09-20 04:47:47 -0400

    Remove chat stuff; re-add routes

--- playlist.js
+++ playlist.js
@@ -1,41 +1,19 @@
+Router.map(function() {
+  this.route('home', {path: '/'});
+  this.route('mixtape');
+});
+
 if (Meteor.isClient) {
 
-  Template.rdio_account.events({
-    'click .sign-in': function () {
-      alert('yup');
-    }
-  });
 
   Template.Songs.songs = function () {
     return Songs.find({}, { sort: { upvotes: 1}});
   }
 
-  /*
   Template.messages.messages = function () {
     return Messages.find({}, { sort: { time: -1 }});
   };
 
-  
-  Template.input.events = {
-  'keydown input#message' : function (event) {
-      if (event.which == 13) { // 13 is the enter key event
-        var name = 'Anonymous';  
-        var message = document.getElementById('message');
-   
-        if (message.value != '') {
-          Messages.insert({
-            name: name,
-            message: message.value,
-            time: Date.now()
-          });
-   
-          document.getElementById('message').value = '';
-          message.value = '';
-        }
-      }
-    }
-  }
-  */
 }
 
 if (Meteor.isServer) {
