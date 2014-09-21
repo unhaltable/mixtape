2ca6c5122f71c673a8459db2aaca7cda20fe1f13
Spencer Elliott <me@elliottsj.com>
2014-09-21 04:51:56 -0400

    Fix playback

--- scripts/mixtape.js
+++ scripts/mixtape.js
@@ -1,14 +1,14 @@
 if (Meteor.isServer) {
-  function getHostRdio() {
-    var user = Mixtapes.findOne(Session.get('mixtapeId')).host;
+  function getHostRdio(mixtapeId) {
+    var user = Meteor.users.findOne(Mixtapes.findOne(mixtapeId).host._id);
     var rdio = Rdio.forUser(user);
     if (!rdio) return "No Rdio credentials";
     return rdio;
   }
 
   Meteor.methods({
-    getPlaybackToken: function () {
-      return getHostRdio().call('getPlaybackToken', {domain: 'localhost'});
+    getPlaybackToken: function (mixtapeId) {
+      return getHostRdio(mixtapeId).call('getPlaybackToken', {domain: 'localhost'});
     },
 
     rdioCall: function (method, params) {
@@ -27,54 +27,7 @@ if (Meteor.isClient) {
     Session.set('clientID', tempID);
   });
 
-  var mixtapesHandle = Meteor.subscribe('mixtapes', function () {
-    Meteor.call('getPlaybackToken', function (error, result) {
-      var playbackToken = result;
-      var $nowPlaying = $('#now-playing');
-
-      $nowPlaying.bind('ready.rdio', function () {
-        // Rdio widget is ready
-        $(this).rdio().play('t22410658');
-      });
-
-      $nowPlaying.bind('playingTrackChanged.rdio', function (e, playingTrack, sourcePosition) {
-        if (playingTrack) {
-          duration = playingTrack.duration;
-          $('#art').attr('src', playingTrack.icon);
-          $('#track').text(playingTrack.name);
-          $('#album').text(playingTrack.album);
-          $('#artist').text(playingTrack.artist);
-        }
-      });
-      $nowPlaying.bind('positionChanged.rdio', function (e, position) {
-        $('#position').css('width', Math.floor(100 * position / duration) + '%');
-      });
-      $nowPlaying.bind('playStateChanged.rdio', function (e, playState) {
-        if (playState == 0) { // paused
-          $('#play').show();
-          $('#pause').hide();
-        } else {
-          $('#play').hide();
-          $('#pause').show();
-        }
-      });
-
-      $nowPlaying.rdio(playbackToken);
-
-      $('#previous').click(function () {
-        $('#api').rdio().previous();
-      });
-      $('#play').click(function () {
-        $('#api').rdio().play();
-      });
-      $('#pause').click(function () {
-        $('#api').rdio().pause();
-      });
-      $('#next').click(function () {
-        $('#api').rdio().next();
-      });
-    });
-  });
+  var mixtapesHandle = Meteor.subscribe('mixtapes');
 
   var songsHandle;
   // Always be subscribed to the songs for the selected mixtape.
@@ -93,7 +46,6 @@ if (Meteor.isClient) {
     if (!mixtapeId)
       return [];
 
-    debugger;
     return Songs.find({ mixtapeId: mixtapeId }, {sort: {votes: 'desc'}});
   };
 
@@ -170,9 +122,56 @@ if (Meteor.isClient) {
   });
 
   Template.mixtape.rendered = function () {
-    Meteor.subscribe('mixtapeId', function () {
+    Meteor.subscribe('mixtapes', function () {
       var mixtapeTag = Router.current().data().tag;
       Session.set('mixtapeId', Mixtapes.findOne({ tag: mixtapeTag })._id);
+
+      Meteor.call('getPlaybackToken', Session.get('mixtapeId'), function (error, result) {
+        var playbackToken = result;
+        var $nowPlaying = $('#now-playing');
+
+        $nowPlaying.bind('ready.rdio', function () {
+          // Rdio widget is ready
+          $(this).rdio().play('t22410658');
+        });
+
+        $nowPlaying.bind('playingTrackChanged.rdio', function (e, playingTrack, sourcePosition) {
+          if (playingTrack) {
+            duration = playingTrack.duration;
+            $('#art').attr('src', playingTrack.icon);
+            $('#track').text(playingTrack.name);
+            $('#album').text(playingTrack.album);
+            $('#artist').text(playingTrack.artist);
+          }
+        });
+        $nowPlaying.bind('positionChanged.rdio', function (e, position) {
+          $('#position').css('width', Math.floor(100 * position / duration) + '%');
+        });
+        $nowPlaying.bind('playStateChanged.rdio', function (e, playState) {
+          if (playState == 0) { // paused
+            $('#play').show();
+            $('#pause').hide();
+          } else {
+            $('#play').hide();
+            $('#pause').show();
+          }
+        });
+
+        $nowPlaying.rdio(playbackToken);
+
+        $('#previous').click(function () {
+          $('#api').rdio().previous();
+        });
+        $('#play').click(function () {
+          $('#api').rdio().play();
+        });
+        $('#pause').click(function () {
+          $('#api').rdio().pause();
+        });
+        $('#next').click(function () {
+          $('#api').rdio().next();
+        });
+      });
     });
   };
 
